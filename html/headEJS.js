d24166dcd0b079160ab6eed0392cd78338e4016d
Jack Frost <albarrankevin97@gmail.com>
2020-03-29 23:28:22 -0700

    finished html content for head.ejs file

---
+++ html/headEJS.js
@@ -0,0 +1,19 @@
+const logger = require("../logger/logger")("head.ejs");
+const htmlScripts = [
+	"<title> Test Title </title>",
+	'\n<meta charset="utf-8" />',
+	'\n<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">',
+	"\n\n<meta",
+	'\n    name="description',
+	'\n    content="some content goes here"',
+	"\n/>",
+	"\n<meta",
+	'\n    name="keywords"',
+	'\n    content="keywords to search by"',
+	"\n/>",
+	'\n<meta name="author" content="Kevin Albarran" />',
+	'\n<meta name="robots" content="follow" />',
+	'\n\n<%- include("./css.ejs") %>'
+];
+
+htmlScripts.forEach((str) => logger.write(str));
